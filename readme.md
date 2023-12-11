9cdcbd0949a3d9b90f8693bb7bf47b52b8b6cf9b
unknown <r.rafi1615005@gmail.com>
2023-12-11 20:47:07 +0600

    project-initialized

---
+++ readme.md
@@ -0,0 +1,7 @@
+1. clone the repositry to your local computer: https://github.com/nishat-md-r-rafi/assignment-2
+
+2. run command: npm install
+
+3. add a ".env" file having environment varible specified in .env.example
+
+4. run command: npm start
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json('He aaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+});
+
+export default app;
